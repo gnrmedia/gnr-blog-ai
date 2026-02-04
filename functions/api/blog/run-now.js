92e70117e0e0561b61f784addd9cf6b2ee8d6d17
GNR Media <admin@gnrmedia.global>
2026-02-04 18:49:58 +1100

    Fix import path for run-now handler

--- functions/api/blog/run-now.js
+++ functions/api/blog/run-now.js
@@ -1,4 +1,4 @@
-import { handleRunNow } from "../../_lib/blog-handlers.js";
+import { handleRunNow } from "../../../_lib/blog-handlers.js";
 
 export async function onRequest(context) {
   // context = { request, env, params, waitUntil, next, data }
